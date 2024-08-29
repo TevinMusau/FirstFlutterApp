afed8fe4242ffca41c50c5d8c1094ab1d3e63a2d
Kendi <kendinatalie17@gmail.com>
2024-08-29 15:12:21 +0300

    Stateless Widgets, Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First Flutter App'),
-      centerTitle: true,
-      backgroundColor: Colors.blue[400],
-    ), // AppBar
-    body: Center(
-      child: Text(
-        'Hello Hello',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-        ),
-      ),
-    ), // Center
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('data'),
-      backgroundColor: Colors.red[400],
-    ), // floating Action Button
-  ), // Scaffold
+  home: Home(),
 )); // MaterialApp
+
+// Stateless Widget
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: Center(
+        child: Image.asset('assets/muichiro.jpg')
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('data2'),
+        backgroundColor: Colors.red[400],
+      ), // floating Action Button
+    );
+  }
+}
+
 
 
